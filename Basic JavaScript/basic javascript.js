03f98ada38da605230aba1ac2c0ef822a01242ce
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-10-21 11:35:32 -0700

    provided

--- Basic JavaScript/basic javascript.js
+++ Basic JavaScript/basic javascript.js
@@ -1,5 +1,20 @@
-function functionWithArgs (a, b) {
-    console.log(a+b);
-}
-functionWithArgs(1, 2);
-functionWithArgs(7, 9);
+// Declare the myGlobal variable below this line
+
+
+function fun1() {
+    // Assign 5 to oopsGlobal Here
+  
+  }
+  
+  // Only change code above this line
+  
+  function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+      output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+      output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+  }
