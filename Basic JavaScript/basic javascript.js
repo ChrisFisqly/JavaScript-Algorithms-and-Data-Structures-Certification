109d5aba3c36288a927cb3dafb004cf855cb4198
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-10-21 06:14:28 -0700

    Word Blanks

--- Basic JavaScript/basic javascript.js
+++ Basic JavaScript/basic javascript.js
@@ -1,5 +1,8 @@
-// Setup
-var lastName = "Lovelace";
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
 
 // Only change code below this line
-var secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+var wordBlanks = "My " + myNoun + " is " + myAdjective + " and " + myVerb + " away " + myAdverb +"."; // Change this line
+// Only change code above this line
