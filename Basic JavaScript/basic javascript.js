10913688a625d14bdc7b3af321737e19999129c9
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-10-22 07:25:56 -0700

    Return a Value from a FunctionUnderstanding Undefined Value returned from a Function with Return

--- Basic JavaScript/basic javascript.js
+++ Basic JavaScript/basic javascript.js
@@ -1,6 +1,16 @@
-function timesFive(num) {
-  return num * 5;
+// Setup
+var sum = 0;
+
+function addThree() {
+  sum = sum + 3;
 }
-var answer = timesFive(5); // 25
-var answer = timesFive(2); // 10
-var answer = timesFive(0); // 0
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
