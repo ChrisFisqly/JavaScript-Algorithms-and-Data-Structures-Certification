bcd41b1aaeff89e90f68144cdc77ee86b9f7249f
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-10-24 19:31:56 -0700

    Stand in Line

--- Basic JavaScript/basic javascript.js
+++ Basic JavaScript/basic javascript.js
@@ -1,10 +1,17 @@
-// Setup
-var processed = 0;
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item); //2
+  var removed = arr.shift();   //3
+ return removed;   //4
+  return item;
 
-function processArg(num) {
-  return (num + 3) / 5;
+
 }
 
-// Only change code below this line
+// Setup
+var testArr = [1,2,3,4,5];
 
-processed = processArg(7);
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
