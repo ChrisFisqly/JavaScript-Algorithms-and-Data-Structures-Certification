434fb66462c9c645197b0876c3f0147fde42ff65
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-11-06 06:32:00 -0800

    su

--- Basic JavaScript/basic javascript.js
+++ Basic JavaScript/basic javascript.js
@@ -1,15 +1,13 @@
-function testElse(val) {
-  var result = "";
-  // Only change code below this line
-
-  if (val > 5) {
-    result = "Bigger than 5";
-  } else {
-    result = "5 or Smaller";
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
   }
 
-  // Only change code above this line
-  return result;
+  if (val < 5) {
+    return "Smaller than 5";
+  }
+
+  return "Between 5 and 10";
 }
 
-testElse(4);
+testElseIf(7);
