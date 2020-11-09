c03b4100b1c1cdf1e26b20efc1e89e4cb3371e27
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-11-09 06:26:14 -0800

    s

--- Basic JavaScript/basic javascript.js
+++ Basic JavaScript/basic javascript.js
@@ -1,6 +1,9 @@
 function testSize(num) {
   // Only change code below this line
 
+    if (Tiny < 5) {
+      return "Tiny"
+    } else if 
 
   return "Change Me";
   // Only change code above this line
