facf0b373e4bf6cd5945774cadb2b6fbf3b8aa4f
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-11-12 09:37:48 -0800

    par

--- Basic JavaScript/basic javascript.js
+++ Basic JavaScript/basic javascript.js
@@ -5,6 +5,7 @@ function golfScore(par, strokes) {
   if (strokes === 1) return "Hole-in-one!";
   else if ((strokes - par) <= -2) return 'Eagle';
   else if ((strokes - par) === -1) return 'Birdie';
+  else if (strokes === par) return 'Par';
 
   return "Change Me";
   // Only change code above this line
