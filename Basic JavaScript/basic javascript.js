ab2a67b0c46945de527957e41c266098944e052b
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-11-13 06:49:37 -0800

    case1

--- Basic JavaScript/basic javascript.js
+++ Basic JavaScript/basic javascript.js
@@ -2,7 +2,9 @@ function caseInSwitch(val) {
   var answer = "";
   // Only change code below this line
 
-
+  switch (val) {
+    case 1:
+      answer = "alpha";
 
   // Only change code above this line
   return answer;
