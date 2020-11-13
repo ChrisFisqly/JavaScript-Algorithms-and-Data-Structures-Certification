6c5f0dc91e88b1a7c79efd7a180237e3291af73a
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-11-13 06:45:15 -0800

    su

--- Basic JavaScript/basic javascript.js
+++ Basic JavaScript/basic javascript.js
@@ -1,17 +1,11 @@
-var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
-function golfScore(par, strokes) {
+function caseInSwitch(val) {
+  var answer = "";
   // Only change code below this line
 
-  if (strokes === 1) return "Hole-in-one!";
-  else if ((strokes - par) <= -2) return 'Eagle';
-  else if ((strokes - par) === -1) return 'Birdie';
-  else if (strokes === par) return 'Par';
-  else if ((strokes - par) === 1) return 'Bogey';
-  else if ((strokes - par) === 2) return 'Double Bogey';
-  else return 'Go Home!';
 
-  
+
   // Only change code above this line
+  return answer;
 }
 
-golfScore(5, 4);
+caseInSwitch(1);
