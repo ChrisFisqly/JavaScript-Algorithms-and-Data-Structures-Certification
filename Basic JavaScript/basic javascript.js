ad4da0fd360b6f2fb4962a523af20533397e4d04
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-11-15 09:14:11 -0800

    su

--- Basic JavaScript/basic javascript.js
+++ Basic JavaScript/basic javascript.js
@@ -1,24 +1,11 @@
-function caseInSwitch(val) {
+function switchOfStuff(val) {
   var answer = "";
   // Only change code below this line
 
-  switch (val) {
-      case 1:
-        answer = "alpha";
-        break;
-      case 2:
-        answer = "beta";
-        break;
-      case 3:
-        answer = "gamma";
-        break;
-      case 4:
-        answer = "delta";
-        break;
-      }    
+
 
   // Only change code above this line
   return answer;
 }
 
-caseInSwitch(1);
+switchOfStuff(1);
