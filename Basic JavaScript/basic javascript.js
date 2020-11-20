dc971dc740aedc0291b409c0946b92d4fcdb866a
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-11-20 08:03:16 -0800

    low

--- Basic JavaScript/basic javascript.js
+++ Basic JavaScript/basic javascript.js
@@ -1,7 +1,12 @@
 function sequentialSizes(val) {
   var answer = "";
   // Only change code below this line
-
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
 
 
   // Only change code above this line
