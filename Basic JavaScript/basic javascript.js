0647e6adbca9a44e13188857ec5108b6dd62e5fe
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-11-22 09:32:20 -0800

    high

--- Basic JavaScript/basic javascript.js
+++ Basic JavaScript/basic javascript.js
@@ -11,8 +11,13 @@ function sequentialSizes(val) {
     case 5:
     case 6:
      answer = "Mid";
-     break;
-
+    break;
+    case 7:
+    case 8:
+    case 9:
+    answer = "High";
+    break;
+  }
   // Only change code above this line
   return answer;
 }
