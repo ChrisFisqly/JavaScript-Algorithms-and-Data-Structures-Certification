5f1704183ef027498bd9b138499835b072fffcf0
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-11-23 06:37:30 -0800

    su

--- Basic JavaScript/basic javascript.js
+++ Basic JavaScript/basic javascript.js
@@ -1,25 +1,21 @@
-function sequentialSizes(val) {
+function chainToSwitch(val) {
   var answer = "";
   // Only change code below this line
-  switch (val) {
-    case 1:
-    case 2:
-    case 3:
-      answer = "Low";
-      break;
-    case 4:
-    case 5:
-    case 6:
-     answer = "Mid";
-    break;
-    case 7:
-    case 8:
-    case 9:
-    answer = "High";
-    break;
+
+  if (val === "bob") {
+    answer = "Marley";
+  } else if (val === 42) {
+    answer = "The Answer";
+  } else if (val === 1) {
+    answer = "There is no #1";
+  } else if (val === 99) {
+    answer = "Missed me by this much!";
+  } else if (val === 7) {
+    answer = "Ate Nine";
   }
+
   // Only change code above this line
   return answer;
 }
 
-sequentialSizes(1);
+chainToSwitch(7);
