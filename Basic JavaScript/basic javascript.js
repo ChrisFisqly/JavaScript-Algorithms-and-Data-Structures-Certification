5cec1fce9e57436a6c09a90f78ef6a82d0d41787
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-12-07 06:44:38 -0800

    su

--- Basic JavaScript/basic javascript.js
+++ Basic JavaScript/basic javascript.js
@@ -1,27 +1,11 @@
-function chainToSwitch(val) {
-  var answer = "";
+function isLess(a, b) {
   // Only change code below this line
-
-  switch (val) {
-    case "bob":
-      answer = "Marley";
-      break;
-    case 42:
-      answer = "The Answer";
-      break;
-    case 1:
-      answer = "There is no #1";
-      break;
-    case 99:
-      answer = "Missed me by this much!";
-      break;
-    case 7:
-      answer = "Ate Nine";
-      break;
-    }
-
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
   // Only change code above this line
-  return answer;
 }
 
-chainToSwitch(7); //complete
+isLess(10, 15);
